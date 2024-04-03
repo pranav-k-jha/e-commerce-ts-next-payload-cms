55776e7bc884b20d6cedc8089283cb5eef126bf4
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-03 15:47:01 -0400

    added button component in the Nav bar

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
@@ -6,17 +8,23 @@ import classes from './index.module.scss'
 import Image from 'next/image'
 import { HeaderNav } from '../Nav'
 import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
   return (
-    <nav className={classes.header}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter className={classes.wrap}>
         <Link href="/">
-          <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
-Ï
-        <HeaderNav header={header}/>
-        <MobileNav header={header}/>
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
