f06201ad311abef9a0fbb928f77ff4ee73dded74
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-03 11:32:25 -0400

    added mobile navigation as a component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -2,13 +2,21 @@ import Link from 'next/link'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 
+import classes from './index.module.scss'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
+
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
-    <nav>
-      <Gutter>
+    <nav className={classes.header}>
+      <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" />
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
         </Link>
+Ï
+        <HeaderNav header={header}/>
+        <MobileNav header={header}/>
       </Gutter>
     </nav>
   )
