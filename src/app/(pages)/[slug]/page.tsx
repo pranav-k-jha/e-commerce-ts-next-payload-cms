cc74b83ef5577e9b63b2122534ee7278162330b4
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-06 15:48:56 -0400

    updated hero section

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -58,9 +58,11 @@ export default async function Page({ params: { slug = 'home' } }) {
   return (
     <React.Fragment>
       {slug === 'home' ? (
-        <Gutter>
+        <section>
           <Hero {...hero} />
-        </Gutter>
+
+          <Gutter>{<></>}</Gutter>
+        </section>
       ) : (
         <>
           <Hero {...hero} />
