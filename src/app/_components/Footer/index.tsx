12bf29ed793f3bea89e68b45ccc0f6963e1695f8
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-04-05 23:56:34 -0400

    resolved conflict between footer import and function

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-import { Footer } from '../../../payload/payload-types'
+import { Footer as FooterType } from '../../../payload/payload-types'
 import { fetchFooter } from '../../_api/fetchGlobals'
 import FooterComponent from './FooterComponent'
 
 import classes from './index.module.scss'
 
 export async function Footer() {
-  let footer: Footer | null = null
+  let footer: FooterType | null = null
 
   try {
     footer = await fetchFooter()
   } catch (error) {
+    console.error('Failed to fetch footer data:', error)
   }
-
   const navItems = footer?.navItems || []
 
   return (
